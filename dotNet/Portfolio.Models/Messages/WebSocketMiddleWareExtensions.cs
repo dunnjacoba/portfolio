a9fe2d5caf294fb0f4173ec37e8e6af916d53dbc
Jacob Dunn <101910318+dunnjacoba@users.noreply.github.com>
2022-07-11 22:27:02 -0500

    Add files via upload

---
+++ dotNet/Portfolio.Models/Messages/WebSocketMiddleWareExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Sabio.Web.Api.StartUp.Messages
+{
+    public static class WebSocketMiddleWareExtensions
+    {       
+        public static IApplicationBuilder UseWebSocketServer(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<WebSocketServerMiddleware>();
+        }
+
+        public static IServiceCollection AddWebSocketManager(this IServiceCollection services)
+        {
+            services.AddSingleton<WebSocketServerConnectionManager>();
+            return services;
+        }
+    }
+}
